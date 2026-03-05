5817f40afd78324cce249824f7af1ec75b97ebf2
khentadelino <khentadelino.dev@gmail.com>
2026-03-05 14:36:18 +0800

    Update Done About & Menu

---
+++ js/reviewSlider.js
@@ -0,0 +1,13 @@
+const slider = tns({
+  container: ".reviewSlider",
+  items: 1,
+  slideBy: 1,
+  speed: 400,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayButtonOutput: false,
+  controls: false,
+  nav: true,
+  navPosition: "bottom",
+  rewind: true,
+});
